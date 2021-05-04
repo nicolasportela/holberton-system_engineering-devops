d430ad01f3de1f7bf671896d7eeabae84111b0e3
nicolasportela <2103@holbertonschool.com>
2021-05-04 00:25:17 +0000

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that for a given employee ID, returns his/her TODO list progress"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    number = sys.argv[1]
+    identityusers = {'id': number}
+    r = requests.get("https://jsonplaceholder.typicode.com/users",
+                     params=identityusers).json()
+    username = r[0].get("username")
+    identitytodos = {'userId': number}
+    r2 = requests.get("https://jsonplaceholder.typicode.com/todos",
+                      params=identitytodos).json()
+
+    tasks = []
+    for task in r2:
+        task_dict = {}
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict["username"] = username
+        tasks.append(task_dict)
+    jsonobj = {}
+    jsonobj[number] = tasks
+    with open("{}.json".format(number), 'w') as jsonfile:
+        json.dump(jsonobj, jsonfile)
