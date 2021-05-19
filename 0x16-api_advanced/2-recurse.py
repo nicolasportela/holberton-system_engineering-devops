ea28078a847668c8c62852359bbffbd507b5b815
nicolasportela <2103@holbertonschool.com>
2021-05-19 03:10:25 +0000

    tasl 2 edited + its main file added

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" Project 0x16. API advanced: Task 1 """
+"""recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], next_page=""):
-    """ Recursive function that queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit,
-    the function should return None."""
+    """returns titles of hot articles"""
 
-    headers = {'User-Agent': 'SoyYo!'}
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     url = "https://www.reddit.com/r/" \
         + subreddit \
         + "/hot.json?after=" \
         + next_page
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code == 200:
-        for element in res.json().get('data').get('children'):
-            hot_list.append(element.get('data').get('title'))
-        next_page = res.json().get('data').get('after')
+    r = requests.get(url, headers=header, allow_redirects=False)
+    if r.status_code != 200:
+        return (None)
+    else:
+        dic = r.json()
+        for item in dic.get('data').get('children'):
+            hot_list.append(item.get('data').get('title'))
+        next_page = dic.get('data').get('after')
         if next_page:
             recurse(subreddit, hot_list, next_page)
         return hot_list
-    else:
-        return None
-    return hot_list
