e13a10ac7811015a83fe18b59eb96a5a26598f1a
nicolasportela <2103@holbertonschool.com>
2021-05-28 00:32:26 +0000

    task 0 fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
 # Issue: Nginx limit to open files is low. Solution: ulimit must be increased (-n flag for open files)
 exec { 'limit':
-command => 'sed -i s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g /etc/default/nginx'
+command => 'sed -i s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g /etc/default/nginx',
+path => '/etc/default/nginx'
 }
