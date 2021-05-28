06eb9b6c0788bbc465d419e470a75aafcbd7160c
nicolasportela <2103@holbertonschool.com>
2021-05-28 00:21:36 +0000

    task 0 fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# added option -l to make ApacheBench accept variable document length
-exec { 'ab -c 100 -n 2000 -l localhost/':
+# Issue: Nginx limit to open files is low. Solution: ulimit must be increased (-n flag for open files)
+exec { 'sed -i s/ULIMIT="-n 15"/ULIMIT="-n 4096"/g /etc/default/nginx && sudo service nginx restart && sudo service nginx reload':
 provider => 'shell'
 }
