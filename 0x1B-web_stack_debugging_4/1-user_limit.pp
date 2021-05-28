b534f1dca4ecd1064435bd21a012032aaf8fc13b
nicolasportela <2103@holbertonschool.com>
2021-05-28 02:17:53 +0000

    task advanced 1 fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
 # setting user holberton limits
-exec { 'sed -i s/4/100000/g /etc/security/limits.conf && sed -i s/5/100000/g /etc/security/limits.conf:
+exec { 'sed -i s/4/100000/g /etc/security/limits.conf && sed -i s/5/100000/g /etc/security/limits.conf':
 provider => 'shell'
 }
